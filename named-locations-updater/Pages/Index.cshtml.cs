af89a90c56b74cb9d5925edc05d3c1b095c89d01
Caleb Seelhoff <cseelhoff@microsoft.com>
2024-02-21 19:09:43 -0600

    initial

---
+++ named-locations-updater/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace named_locations_updater.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}

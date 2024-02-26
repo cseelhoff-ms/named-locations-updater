2a5e07714697c384d9507764616b14ce8e0cd6e2
Caleb Seelhoff <cseelhoff@microsoft.com>
2024-02-26 11:38:49 -0600

    AuthorizeForScopes

--- WebApplication4/Pages/Index.cshtml.cs
+++ WebApplication4/Pages/Index.cshtml.cs
@@ -5,9 +5,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using Microsoft.Graph;
 using Microsoft.Graph.Models;
+using Microsoft.Identity.Web;
 
 namespace WebApplication4.Pages
 {
+    [AuthorizeForScopes(ScopeKeySection = "MicrosoftGraph:Scopes")]
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
@@ -17,7 +19,7 @@ namespace WebApplication4.Pages
         public IndexModel(ILogger<IndexModel> logger, GraphServiceClient graphServiceClient)
         {
             _logger = logger;
-            List<String> scopes = new List<String>(new[] { "https://graph.microsoft.com/.default" });
+            string[] scopes = new[] { "https://graph.microsoft.com/.default" };
             _managedIdentityCredential = new ManagedIdentityCredential();
             _graphServiceClient = new GraphServiceClient(_managedIdentityCredential, scopes);
             Locations = new List<NamedLocation>();
@@ -27,15 +29,15 @@ namespace WebApplication4.Pages
 
         public async Task OnGetAsync()
         {
-            List<String> scopes = new List<String>(new[] { "https://graph.microsoft.com/.default" });
-            GraphServiceClient graphServiceClient = new GraphServiceClient(_managedIdentityCredential, scopes);
+            string[] scopes = new[] { "https://graph.microsoft.com/.default" };
+            // GraphServiceClient graphServiceClient = new GraphServiceClient(_managedIdentityCredential, scopes);
             // log print all debug log information for GraphServiceClient graphServiceClient including principal id, scopes, permissions, ect...
-            _logger.LogDebug("GraphServiceClient graphServiceClient: {graphServiceClient}", graphServiceClient);
-            _logger.LogDebug("GraphServiceClient graphServiceClient: {graphServiceClient}", _graphServiceClient);
-            _logger.LogDebug("ManagedIdentityCredential _managedIdentityCredential: {managedIdentityCredential}", _managedIdentityCredential);
-            _logger.LogDebug("Scopes: {scopes}", scopes);
+            //_logger.LogDebug("GraphServiceClient graphServiceClient: {graphServiceClient}", graphServiceClient);
+            //_logger.LogDebug("GraphServiceClient graphServiceClient: {graphServiceClient}", _graphServiceClient);
+            //_logger.LogDebug("ManagedIdentityCredential _managedIdentityCredential: {managedIdentityCredential}", _managedIdentityCredential);
+            //_logger.LogDebug("Scopes: {scopes}", scopes);
 
-            NamedLocationCollectionResponse? result = await graphServiceClient.Identity.ConditionalAccess.NamedLocations.GetAsync();
+            NamedLocationCollectionResponse? result = await _graphServiceClient.Identity.ConditionalAccess.NamedLocations.GetAsync();
             Locations = result?.Value ?? new List<NamedLocation>();
         }
     }
